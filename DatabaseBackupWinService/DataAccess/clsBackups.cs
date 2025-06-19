611af300004241fdef3285d933a16c3c97a6dc23
Marwan Alawlaqi <tlooddi107@gmail.com>
2025-06-19 15:32:12 +0300

    Add files via upload

---
+++ DatabaseBackupWinService/DataAccess/clsBackups.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class clsBackups
+    {
+
+        /// <summary>
+        /// This for backup the database with differential
+        /// </summary>
+        /// <param name="ToDiskPath">This is the path to save a backup</param>
+        /// <returns>result of backup and the message if there is an error.</returns>
+        public static (bool Result,string MessageError) BackUp(string ToDiskPath)
+        {
+            (bool Result, string MessageError) Data = (true, null);
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]))
+                {
+
+                    string query = @"exec SP_BackUp
+                                    @Path";
+                    using(SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        conn.Open();
+                        cmd.Parameters.AddWithValue("@Path", ToDiskPath);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Data.Result = false;
+                Data.MessageError = ex.Message;
+            }
+
+            return Data;
+        }
+    }
+}
